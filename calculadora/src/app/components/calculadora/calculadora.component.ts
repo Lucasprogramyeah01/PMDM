295b3675c1dcc7295df7bda98d350a8b1dfe6dc7
Lucas <falla.urluc24@triana.salesianos.edu>
2024-10-15 21:58:49 +0200

    Calculadora completada

--- calculadora/src/app/components/calculadora/calculadora.component.ts
+++ calculadora/src/app/components/calculadora/calculadora.component.ts
@@ -7,25 +7,73 @@ import { Component } from '@angular/core';
 })
 export class CalculadoraComponent {
   
-  numero1:number;
-  numero2:number;
+  numero1 = 0
+  numero2 = 0;
+  resultado = 0;
 
-  resultado = this
+  suma = false;
+  resta = false;
 
-  addNum(numero){
+  simbolo = '#';
 
+  digito1Listo = false;
+
+  addNum(numero:number){
+    if(this.digito1Listo){
+      this.digito1Listo = false;
+      return this.numero2 = numero;
+    }else{
+      this.digito1Listo = true;
+      return this.numero1 = numero;
+    }
   }
 
   sumar() {
-    
+    this.suma = true;
+    this.resta = false;
+    this.simboloOperacion();
   }
 
   restar() {
+    this.resta = true;
+    this.suma = false;
+    this.simboloOperacion();
+  }
 
+  simboloOperacion(){
+    if((this.suma==true && this.resta==false)){
+      return this.simbolo='+';
+    }else{
+      if((this.suma==false && this.resta==true)){
+        return this.simbolo='-';
+      }else{
+        return this.simbolo='#';
+      }
+    }
   }
 
   realizarOperacion(){
+    switch(this.suma){
+      case true:{
+        return this.resultado = this.numero1 + this.numero2;
+      }
+      case false:{
+        return this.resultado = this.numero1 - this.numero2;
+      }
+    }
+  }
 
+  reiniciarCalculadora(){
+    this.numero1 = 0
+    this.numero2 = 0;
+    this.resultado = 0;
+
+    this.suma = false;
+    this.resta = false;
+
+    this.simbolo = '#';
+
+    this.digito1Listo = false;
   }
 
 }
