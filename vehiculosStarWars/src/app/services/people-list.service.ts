330402abeeeab3c0622bc524a0467cab952f4282
Lucas Falla Urtiaga <falla.urluc24@triana.salesianos.edu>
2024-10-24 12:30:09 +0200

    Proyecto StarWars tocado.

--- vehiculosStarWars/src/app/services/people-list.service.ts
+++ vehiculosStarWars/src/app/services/people-list.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Vehiculo, VehiculoListResponse } from '../interfaces/listaVehiculos.interfaces';
 import { PeopleListResponse } from '../interfaces/people-list.interfaces';
+import { PeopleDeatilsResponse } from '../interfaces/people-detail.interfaces';
 
 @Injectable({
   providedIn: 'root'
@@ -15,4 +16,7 @@ export class PeopleListService {
     return this.http.get<PeopleListResponse>('https://swapi.dev/api/people');
   }
 
+  getOnePeople(): Observable<PeopleDeatilsResponse>{
+    return this.http.get<PeopleDeatilsResponse>('https://swapi.dev/api/people');
+  }
 }
