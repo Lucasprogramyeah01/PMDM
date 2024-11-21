965cdac5812979456e150508db8329943d5606af
Lucas Falla Urtiaga <falla.urluc24@triana.salesianos.edu>
2024-11-21 11:36:32 +0100

    ProjectAutentication actualizado.

--- ProjectAutentication/src/app/services/auth.service.ts
+++ ProjectAutentication/src/app/services/auth.service.ts
@@ -2,9 +2,11 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { CreateRequestTokenResponse } from '../interfaces/create-request-token.interfaces';
+import { CreateSessionResponse } from '../interfaces/create-session.interfaces';
 
 // Screamming snake case
 const API_KEY = '433d2c486572afb242c6fe7c1ddc6771';
+const API_BASE_URL = 'https://api.themoviedb.org/3';
 
 @Injectable({
   providedIn: 'root',
@@ -12,10 +14,20 @@ const API_KEY = '433d2c486572afb242c6fe7c1ddc6771';
 export class AuthService {
   constructor(private http: HttpClient) {}
 
-  // STEP 1
-  createRequestToken(): Observable<CreateRequestTokenResponse> {
+   // STEP 1
+   createRequestToken(): Observable<CreateRequestTokenResponse> {
     return this.http.get<CreateRequestTokenResponse>(
-      `https://api.themoviedb.org/3/authentication/token/new?api_key=${API_KEY}`
+      `${API_BASE_URL}/authentication/token/new?api_key=${API_KEY}`
+    );
+  }
+
+  // STEP 3
+  createSession(): Observable<CreateSessionResponse> {
+    return this.http.post<CreateSessionResponse>(
+      `${API_BASE_URL}/authentication/session/new?api_key=${API_KEY}`,
+      {
+        request_token: localStorage.getItem('token'),
+      }
     );
   }
 }
