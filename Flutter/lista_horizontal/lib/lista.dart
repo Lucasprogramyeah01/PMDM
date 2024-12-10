4c28b0aabbaa3af5be8dae95e97f43b6207ec250
Lucas Falla Urtiaga <falla.urluc24@triana.salesianos.edu>
2024-12-10 12:31:36 +0100

    Flutter lista_horizontal empezado.

---
+++ Flutter/lista_horizontal/lib/lista.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class ListaPage extends StatelessWidget {
+  const ListaPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        color: Color(0xFFDFDFE0),
+        width: double.infinity,
+
+      )
+    );
+  }
+}
